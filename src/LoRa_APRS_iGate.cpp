b95f449f8f5b80056ebec0bde22a79c793c28c9f
Daniel Caujolle-Bert <f1rmb.daniel@naboo-homelinux.org>
2022-01-17 13:45:47 +0100

    Bump version.

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -18,7 +18,7 @@
 #include "TaskWifi.h"
 #include "ProjectConfiguration.h"
 
-#define VERSION "22.03.0"
+#define VERSION "22.03.1"
 
 TaskQueue<std::shared_ptr<APRSMessage>> toAprsIs;
 TaskQueue<std::shared_ptr<APRSMessage>> fromModem;
